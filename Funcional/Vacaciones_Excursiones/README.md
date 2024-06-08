f0f222b79a653c426e85864a3288c8a7e01bc337
agustinaduric <130220670+agustinaduric@users.noreply.github.com>
2024-06-08 19:06:18 -0300

    Vacaciones y Excursiones

---
+++ Funcional/Vacaciones_Excursiones/README.md
@@ -0,0 +1,1 @@
+[Enunciado](https://docs.google.com/document/d/1C_oehBaJYavsacmThRZcrpIpX6axxVOdX19vYusRhlE/edit#heading=h.jqehittq4m6)
---
+++ Funcional/Vacaciones_Excursiones/VE.hs
@@ -0,0 +1,167 @@
+import Data.List()
+import Text.Show.Functions()
+
+-- PUNTO 1 --
+
+data Turista = Turista {
+    nivelCansacio :: Int,
+    nivelEstres :: Int,
+    acompaniado :: Bool,
+    idiomas :: [String]
+}
+
+modificarEstres :: Int -> Turista -> Turista
+modificarEstres unaCifra unTurista =
+    unTurista { nivelEstres = nivelEstres unTurista + unaCifra }
+
+modificarCansacio :: Int -> Turista -> Turista
+modificarCansacio unaCifra unTurista =
+    unTurista { nivelCansacio = nivelCansacio unTurista + unaCifra }
+
+type Idioma = String
+
+sumarIdioma :: Idioma -> Turista -> Turista
+sumarIdioma unIdioma unTurista = 
+    unTurista { idiomas = unIdioma : idiomas unTurista }
+
+cambiarCompania :: Turista -> Turista
+cambiarCompania unTurista = 
+    unTurista { acompaniado = not . acompaniado $ unTurista}
+
+type Excursion = Turista -> Turista
+
+irALaPlaya :: Excursion
+irALaPlaya unTurista
+    | acompaniado unTurista = modificarEstres (-1) unTurista
+    | otherwise = modificarCansacio (-10) unTurista
+
+type Paisaje = String
+
+apreciarUn :: Paisaje -> Turista -> Turista
+apreciarUn paisaje = modificarEstres (- (length paisaje))
+
+type Minutos = Int
+
+caminar :: Minutos -> Turista -> Turista
+caminar unosMinutos =
+    modificarEstres (- nivelIntesidad unosMinutos) . modificarCansacio (nivelIntesidad unosMinutos )
+
+type Intensidad = Int
+
+nivelIntesidad ::  Minutos -> Intensidad
+nivelIntesidad minutos = div minutos 4
+
+data Marea = Fuerte | Moderada | Tranquila deriving Eq
+
+paseoEnBarco :: Marea -> Turista -> Turista
+paseoEnBarco unaMarea unTurista 
+    | unaMarea == Fuerte = modificarEstres 6 . modificarCansacio 10 $ unTurista
+    | unaMarea == Tranquila = sumarIdioma "Alemán" . apreciarUn "Mar" . caminar 10 $ unTurista
+    | otherwise = unTurista
+
+ana :: Turista
+ana = Turista 0 21 True ["Español"]
+
+beto :: Turista
+beto = Turista 15 15 False ["Alemán"]
+
+cathi :: Turista
+cathi = sumarIdioma "Catalán" beto 
+
+-- PUNTO 2 --
+
+--- A)
+
+hacerUnaExcursion :: Excursion -> Turista -> Turista
+hacerUnaExcursion unaExcursion unTurista = 
+    modificarEstres (- obtenerPorcentajeDe nivelEstres unTurista) . unaExcursion $ unTurista
+
+obtenerPorcentajeDe :: Indice -> Turista -> Int
+obtenerPorcentajeDe unAspecto = flip div 100 . unAspecto 
+
+--- B)
+
+type Indice = Turista -> Int
+
+deltaSegun :: (a -> Int) -> a -> a -> Int
+deltaSegun f algo1 algo2 = f algo1 - f algo2
+
+deltaExcursionSegun :: Indice-> Turista -> Excursion -> Int
+deltaExcursionSegun unIndice turistaOriginal unaExcursion =
+    deltaSegun unIndice (unaExcursion turistaOriginal) turistaOriginal
+
+--- C)
+
+esExcursionEduccativa :: Turista -> Excursion -> Bool
+esExcursionEduccativa unTurista unaExcursion =
+    1 <= deltaExcursionSegun (length . idiomas) unTurista unaExcursion
+
+excursionesDesestresantes :: Turista -> [Excursion] -> [Excursion]
+excursionesDesestresantes unTurista  =
+    filter (esDesestresante unTurista) 
+
+esDesestresante :: Turista -> Excursion -> Bool
+esDesestresante unTurista = 
+    (3>=) . deltaExcursionSegun nivelEstres unTurista 
+
+-- PUNTO 3 --
+
+type Tour = [Excursion]
+
+tourCompleto :: Tour
+tourCompleto =
+    [apreciarUn "Cascada" . caminar 20 , caminar 20, sumarIdioma "Melmacquiano"]
+
+ladoB :: Excursion -> Tour
+ladoB unaExcursion = [unaExcursion . paseoEnBarco Tranquila, caminar 120]
+
+islaVecina :: Marea -> Tour
+islaVecina unaMarea
+    | unaMarea == Fuerte = [paseoEnBarco unaMarea, apreciarUn "Lago", paseoEnBarco unaMarea]
+    | otherwise = [paseoEnBarco unaMarea, irALaPlaya, paseoEnBarco unaMarea]
+
+--- A)
+
+hacerUnTour :: Tour -> Turista -> Turista
+hacerUnTour unTour unTurista = 
+    foldr hacerUnaExcursion (modificarEstres (cantidadExcursiones unTour) unTurista) unTour
+
+cantidadExcursiones :: Tour -> Int
+cantidadExcursiones = length 
+
+--- B) 
+
+existeTourConviniente :: Turista -> Tour -> Bool
+existeTourConviniente unTurista  = any (esConveniente unTurista)  
+
+esConveniente :: Turista -> Excursion -> Bool
+esConveniente unTurista unaExcursion= 
+    esDesestresante unTurista unaExcursion && consigueCompania unTurista unaExcursion
+
+consigueCompania ::Turista ->  Excursion ->  Bool
+consigueCompania unTurista  =
+    acompaniado . flip hacerUnaExcursion unTurista
+
+--- C) 
+
+type Efectividad = Int
+
+efectividadDeUnTour :: Tour -> [Turista] -> Efectividad
+efectividadDeUnTour unTour unosTuristas =
+    sum (map (flip espiritualidadDeUnTurista unTour) unosTuristas)
+
+type Espiritualidad = Int
+
+espiritualidadDeUnTurista :: Turista -> Tour -> Espiritualidad
+espiritualidadDeUnTurista unTurista unTour
+    | existeTourConviniente unTurista unTour = 
+        sum (perdidaDe nivelCansacio unTurista unTour ++ perdidaDe nivelEstres unTurista unTour)
+    | otherwise = 0
+
+perdidaDe  :: Indice -> Turista -> Tour -> [Int]
+perdidaDe unIndice unTurista = map (deltaExcursionSegun unIndice unTurista) 
+
+-- PUNTO 4 --
+
+visitarInfinitasPlayas :: Tour
+visitarInfinitasPlayas = repeat irALaPlaya
