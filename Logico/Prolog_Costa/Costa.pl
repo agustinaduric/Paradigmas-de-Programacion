3aacda302030e47d54fca8619d86089d006e3476
agustinaduric <130220670+agustinaduric@users.noreply.github.com>
2024-09-03 22:09:02 -0300

    Update Costa

--- Logico/Prolog_Costa/Costa.pl
+++ Logico/Prolog_Costa/Costa.pl
@@ -1,36 +1,38 @@
 % Base de conocimiento %
+
+% comida(comida, precio).
 comida(hamburguesa, 2000).
 comida(panchito, 1500).
 comida(lomito, 2500).
 comida(caramelo, 0).
 
-% atraccion(tipo, atraccion, apto)
+% atraccion(tipo, caracteristicas(atraccion, apto)).
 atraccion(tranquila, caracteristicas(autitosChocadores, chicosYAdultos)).
 atraccion(tranquila, caracteristicas(casaEmbrujada, chicosYAdultos)).
 atraccion(tranquila, caracteristicas(laberinto, chicosYAdultos)).
 atraccion(tranquila, caracteristicas(tobogan, chicos)).
 atraccion(tranquila, caracteristicas(calesita, chicos)).
 
-% tipo atraccion coeficiente
+% atraccion(tipo, caracteristicas(atraccion, coeficiente).
 atraccion(intensa, caracteristicas(barcoPirata, 14)).
 atraccion(intensa, caracteristicas(tazasChinas, 6)).
 atraccion(intensa, caracteristicas(simulador, 2)).
 
-% tipo nombre giros duracion
+% atraccion(tipo, caracteristicas(nombre, giros, duracion)).
 atraccion(montaniaRusa, caracteristicas(abismoMortalRecargada, 3, 134)).
 atraccion(montaniaRusa, caracteristicas(paseoPorElBodque, 0, 45)).
 
-% tipo nombre
+% atraccion(tipo, nombre).
 atraccion(acuatica, caracteristicas(torpedoSalpicon)).
 atraccion(acuatica, caracteristicas(esperoQueHayasTraidoUnaMudaDeRopa)).
 
-% nombre edad plata hambre aburrimiento
+% visitante(nombre, detalles(edad, plata), hambre, aburrimiento).
 visitante(eusebio, detalles(80, 3000), 50, 0 ).
 visitante(carmela, detalles(80, 0), 0, 25).
 visitante(tomas, detalles(28, 500), 0, 0).
 visitante(morena, detalles(36, 1000), 40, 30).
 
-% nombre grupo
+% grupo(nombre, grupo).
 grupo(eusebio, viejitos).
 grupo(carmela, viejitos).
 
@@ -88,10 +90,46 @@ sacaHambre(Visitante, Comida) :-
 
 sacaHambre(Visitante, caramelo) :-
     visitante(Visitante),
-    not(puedePagar(Visitante, _)).
+    forall(comida(Comida), not((puedePagar(Visitante, Comida), Comida \= caramelo))).
 
 satisfaceSegun(_, _, lomito).
+
 satisfaceSegun(_, Hambre, hamburguesa) :-
     Hambre =< 50.
+
 satisfaceSegun(detalles(Edad, _), _, panchito) :-
     Edad < 18.
+
+% Punto 4 % 
+
+lluviaDeHamburguesas(Visitante, Atraccion) :-
+    puedePagar(Visitante, hamburguesa),
+    atraccionHamburguesa(Visitante, Atraccion).
+
+atraccionHamburguesa(_, Atraccion) :-
+    atraccion(intensa, caracteristicas(Atraccion, Lanzamiento)),
+    Lanzamiento > 10.
+
+atraccionHamburguesa(_, tobogan).
+
+atraccionHamburguesa(Visitante, Atraccion) :-
+    visitante(Visitante, detalles(Edad, _), _, _),
+    montaniaPeligrosa(Visitante, Edad, Atraccion).
+
+montaniaPeligrosa(_, Edad, Atraccion) :-
+    Edad > 18,
+    atraccion(montaniaRusa, caracteristicas(Atraccion, Giros, _)),
+    not((atraccion(montaniaRusa, caracteristicas(OtraAtraccion, OtrosGiros, _)), OtraAtraccion \= Atraccion, OtrosGiros > Giros)).
+
+montaniaPeligrosa(Visitante, Edad, _) :-
+    Edad > 18,
+    not(bienestar(Visitante, necesitaEntretenerse)).
+
+montaniaPeligrosa(_, Edad, Atraccion) :-
+    Edad =< 18, 
+    atraccion(montaniaRusa, caracteristicas(Atraccion, _, Duracion)),
+    Duracion >= 60.
+
+% TODO: Fix repetición de lógica en Edad =< 18 y Edad > 18.
+% TODO: Fix repetición de lógica en manejo Montaña rusa Punto 4.
+% TODO: Punto 5.
